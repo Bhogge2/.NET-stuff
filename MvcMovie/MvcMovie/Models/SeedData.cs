57ea91332d1dfb0ec53b516291fa08aba427dfba
Bhogge2 <hog15005@byui.edu>
2020-04-04 13:49:12 -0600

    updated

---
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "God's Army",
+                        ReleaseDate = DateTime.Parse("1999-7-21"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "17 Miracles",
+                        ReleaseDate = DateTime.Parse("2011-5-25"),
+                        Genre = "Drama",
+                        Rating = "PG-13",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Singles Ward",
+                        ReleaseDate = DateTime.Parse("2002-2-23"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Singles 2nd Ward",
+                        ReleaseDate = DateTime.Parse("2007-8-05"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 3.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Meet the Mormons",
+                        ReleaseDate = DateTime.Parse("2015-4-30"),
+                        Genre = "Documentary",
+                        Rating = "G",
+                        Price = 1.99M
+                    }
+
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
